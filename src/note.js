89b2adcf45a811fb9e63fcc6b499a83e3588ec72
comountainclimber <mlasky46@gmail.com>
2016-02-29 17:48:00 -0700

    ...

--- src/note.js
+++ src/note.js
@@ -1,11 +1,17 @@
 var Note = React.createClass({
+  getInitialState: function(){
+    return {editing: false}
+  },
   edit: function() {
-    alert('editing note');
+    this.setState({editing:true});
+  },
+  save: function() {
+    this.setState({editing:false})
   },
   remove: function() {
     alert('removing note');
   },
-  render: function(){
+  renderDisplay: function() {
     return (
       <div className="note">
         <p>{this.props.children}</p>
@@ -14,7 +20,23 @@ var Note = React.createClass({
           <button onClick={this.remove} className="btn btn-danger glyphicon glyphicon-trash"/>
         </span>
       </div>
+      );
+  },
+  renderForm: function() {
+    return (
+      <div className="note">
+      <textarea defaultValue={this.props.children} className="form-control" />
+      <button onClick={this.save} className="btn btn-success btn-sm glyphicon glyphicon-floppy-disk" />
+      </div>
       )
+  },
+  render: function(){
+    if (this.state.editing) {
+      return this.renderForm()
+    }
+    else {
+      return this.renderDisplay()
+    }
   }
 });
 
